21140f5c960decad5fe03a26d2bc3aa3c3a6e1d1
Kamalen <tidiusff@gmail.com>
2025-01-11 11:55:47 +0100

    Initial commit

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import * as wol from 'wake_on_lan';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+  powerOn() {
+
+  }
+}
